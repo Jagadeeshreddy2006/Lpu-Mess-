9bbf67bf592a31dcd11bdb53d31527daf9ebcac8
jackpot <jagaking894@gmail.com>
2025-12-05 09:13:02 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,29 @@
-self.addEventListener("install", (e) => {
-  console.log("Service Worker Installed");
-  e.waitUntil(
-    caches.open("mess-pass-cache").then((cache) => {
-      return cache.addAll([
-        "/",
-        "/index.html",
-        "/manifest.json"
-      ]);
-    })
+// Simple cache-first service worker just to satisfy PWA requirement
+const CACHE_NAME = "mess-pass-cache-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
   );
 });
 
-self.addEventListener("fetch", (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request))
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+      )
+    )
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then(resp => resp || fetch(event.request))
   );
 });
