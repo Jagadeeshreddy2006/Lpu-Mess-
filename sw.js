edce7ba5ec36390c62cf7ab8fed19fbf7513f5d6
jackpot <jagaking894@gmail.com>
2025-12-05 09:25:22 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,29 +1,13 @@
-// Simple cache-first service worker just to satisfy PWA requirement
-const CACHE_NAME = "mess-pass-cache-v1";
-const ASSETS = [
-  "./",
-  "./index.html",
-  "./manifest.json"
-];
+// sw.js - minimal service worker just to make the app installable
 
 self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
-  );
+  console.log("Service worker installed");
+  // You can skip waiting if you want immediate activation:
+  self.skipWaiting();
 });
 
 self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
-      )
-    )
-  );
+  console.log("Service worker activated");
 });
 
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then(resp => resp || fetch(event.request))
-  );
-});
+// No fetch handler needed for installability, but you can add later if you want offline.
